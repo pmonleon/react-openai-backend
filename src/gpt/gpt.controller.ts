a563e7b80cacaa480812581e83e4d33a3642009e
Francisco Monleon <paco.monleon@octoenergy.com>
2025-02-24 16:45:28 +0100

    pros-cons-discusser and pros-cons-discusser-stream

--- src/gpt/gpt.controller.ts
+++ src/gpt/gpt.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Res, HttpStatus } from '@nestjs/common';
 import { GptService } from './gpt.service';
-import { OrthographyDto } from './dtos';
+import { OrthographyDto, ProsConsDiscusserDto } from './dtos';
+import { Response } from 'express';
 
 @Controller('gpt')
 export class GptController {
@@ -15,5 +16,32 @@ export class GptController {
     return this.gptService.orthographyCheck(orthographyDto);
   }
 
+  @Post('pros-cons-discusser')
+  prosConsDicusser(
+    @Body() prosConsDiscusserDto: ProsConsDiscusserDto,
+  ) {
+    return this.gptService.prosConsDicusser(prosConsDiscusserDto);
+  }
+
+  @Post('pros-cons-discusser-stream')
+  async prosConsDicusserStream(
+    @Body() prosConsDiscusserDto: ProsConsDiscusserDto,
+    @Res() res: Response,
+  ) {
+     const stream = await this.gptService.prosConsDicusserStream(prosConsDiscusserDto);
+
+  
+    res.setHeader('Content-Type', 'application/json');
+    res.status( HttpStatus.OK );
+
+    for await( const chunk of stream ) {
+      const piece = chunk.choices[0].delta.content || '';
+     // console.log(piece);
+      res.write(piece);
+    }
+
+    res.end();
+
+  }
 
 }
